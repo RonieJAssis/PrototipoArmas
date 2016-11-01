7c11d75ef9334d2ecb7b1b241f7c37d637b610ca
RonieJAssis <ronie.gremio@gmail.com>
2016-11-01 20:56:03 -0200

    Animações e tiro

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,52 +2,66 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
 
-	private Animator anim;
-	private Rigidbody2D rb2d;
-    
-    private bool arma1 = false ;
-    private bool arma2 = false ;
-    private bool armado1 = false ;
-    private bool armado2 = false ;
-    private bool desarmado= true ;
-    
+    private Animator anim;
+    private Rigidbody2D rb2d;
+
+    private bool arma1 = false;
+    private bool arma2 = false;
+    private bool armado1 = false;
+    private bool armado2 = false;
+    private bool desarmado = true;
+    private float ShootingRate = 0.1f;
+    private float ShootCooldown = 0f;
+    public Transform SpawnBullet;
+    public GameObject Bullet1;
+    public GameObject Bullet2;
+
     public Transform posPe;
-	[HideInInspector] public bool tocaChao = false;
+    [HideInInspector]
+    public bool tocaChao = false;
 
 
-	public float Velocidade;
-	public float ForcaPulo = 1000f;
-	[HideInInspector] public bool viradoDireita = true;
-	private bool pula = false;
-  
+    public float Velocidade;
+    public float ForcaPulo = 1000f;
+    [HideInInspector]
+    public bool viradoDireita = true;
+    private bool pula = false;
 
-	void Start () {
-		anim = GetComponent<Animator> ();
-		rb2d = GetComponent<Rigidbody2D> ();
 
-      
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+
+
 
 
     }
-	
-	void Update () {
-		//Implementar Pulo Aqui!
-		tocaChao = Physics2D.Linecast(transform.position, posPe.position, 1 << LayerMask.NameToLayer("Chao"));
-		if(Input.GetKeyDown("space") && tocaChao){
-			//pula
-			pula = true;
-            
+
+    void Update()
+    {
+        //Implementar Pulo Aqui!
+        tocaChao = Physics2D.Linecast(transform.position, posPe.position, 1 << LayerMask.NameToLayer("Chao"));
+        if (Input.GetKeyDown("space") && tocaChao)
+        {
+            //pula
+            pula = true;
+
 
 
         }
-	}
+    }
 
 
-	void FixedUpdate()
-	{
+    void FixedUpdate()
+    {
+        if (ShootCooldown > 0)
+            ShootCooldown -= Time.deltaTime;
+
         float translationY = 0;
         float translationX = Input.GetAxis("Horizontal") * Velocidade;
         transform.Translate(translationX, translationY, 0);
@@ -101,10 +115,14 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetMouseButtonDown(0) && desarmado == false && armado1 == true && armado2 == false)
         {
             anim.SetTrigger("Atirando 1");
+            Fire1();
+            ShootCooldown = ShootingRate;
         }
         if (Input.GetMouseButtonDown(0) && desarmado == false && armado1 == false && armado2 == true)
         {
             anim.SetTrigger("Atirando 2");
+            Fire2();
+            ShootCooldown = ShootingRate;
         }
 
 
@@ -132,12 +150,12 @@ public class PlayerController : MonoBehaviour {
 
     }
     void Flip()
-	{
-		viradoDireita = !viradoDireita;
-		Vector3 escala = transform.localScale;
-		escala.x *= -1;
-		transform.localScale = escala;
-	}
+    {
+        viradoDireita = !viradoDireita;
+        Vector3 escala = transform.localScale;
+        escala.x *= -1;
+        transform.localScale = escala;
+    }
 
 
     void OnTriggerEnter2D(Collider2D other)
@@ -147,7 +165,7 @@ public class PlayerController : MonoBehaviour {
 
             other.gameObject.SetActive(false);
             arma2 = true;
-            
+
 
         }
         if (other.tag == "Arma1")
@@ -157,16 +175,38 @@ public class PlayerController : MonoBehaviour {
             arma1 = true;
 
         }
-      
+
     }
 
-    
-  
+
+
     void Armado2()
     {
         desarmado = false;
         armado1 = false;
         armado2 = true;
+    }
+    void Fire1()
+    {
+        if (ShootCooldown <= 0f)
+        {
+            if (Bullet1 != null)
+            {
+                var cloneBullet = Instantiate(Bullet1, SpawnBullet.position, Quaternion.identity) as GameObject;
+                cloneBullet.transform.localScale = this.transform.localScale;
+            }
+        }
+    }
+    void Fire2()
+    {
+        if (ShootCooldown <= 0f)
+        {
+            if (Bullet2 != null)
+            {
+                var cloneBullet = Instantiate(Bullet2, SpawnBullet.position, Quaternion.identity) as GameObject;
+                cloneBullet.transform.localScale = this.transform.localScale;
+            }
+        }
     }
 }
 
